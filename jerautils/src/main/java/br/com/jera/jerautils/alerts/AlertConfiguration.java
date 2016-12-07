1fe5ddc00886ec67bd2494eaa772dd350ba3139c
Daivid Silverio <daivid.silverio@gmail.com>
2016-12-07 14:56:39 -0300

    restricted access to alertconfiguration variables

--- jerautils/src/main/java/br/com/jera/jerautils/alerts/AlertConfiguration.java
+++ jerautils/src/main/java/br/com/jera/jerautils/alerts/AlertConfiguration.java
@@ -2,6 +2,7 @@ package br.com.jera.jerautils.alerts;
 
 import android.content.Context;
 import android.support.annotation.IntDef;
+import android.support.annotation.Nullable;
 import android.support.annotation.RestrictTo;
 import android.support.annotation.StringRes;
 import android.support.design.widget.Snackbar;
@@ -17,15 +18,40 @@ import static android.support.annotation.RestrictTo.Scope.GROUP_ID;
 
 public class AlertConfiguration {
 
-    public String title;
-    public String message;
-    public String actionText;
-    public String cancelText;
+    protected String title;
+    protected String message;
+    protected String actionText;
+    protected String cancelText;
 
     @Snackbar.Duration
-    public int duration = LENGTH_SHORT;
+    protected int duration = LENGTH_SHORT;
 
     private AlertConfiguration() {
+    }
+
+    @Nullable
+    public String getTitle() {
+        return title;
+    }
+
+    @Nullable
+    public String getMessage() {
+        return message;
+    }
+
+    @Nullable
+    public String getActionText() {
+        return actionText;
+    }
+
+    @Nullable
+    public String getCancelText() {
+        return cancelText;
+    }
+
+    @Nullable
+    public int getDuration() {
+        return duration;
     }
 
     private AlertConfiguration(AlertConfiguration alertConfiguration) {
